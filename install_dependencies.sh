29c351367163dfe4bef7748775bcdc5da46104c1
chunhp <chunhp@uci.edu>
2021-12-10 22:29:01 -0800

    Fixing the "install_dependencies.sh" to install torch with cuda

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -48,6 +48,7 @@ echo
 echo 'Installing Python "Requirements.txt"'
 echo '------------------------------------'
 pip install -r requirements.txt
+pip install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchaudio==0.10.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
 
 echo
 echo 'Installing Espeak'
